7c5a2ad7d237227076efa1d6714ab757db07f2ed
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2021-12-07 20:50:22 +0700

    feat: update static stock product

--- src/pages/manage.tsx
+++ src/pages/manage.tsx
@@ -2,8 +2,29 @@ import { Container, Table, Form, Row, Col } from "react-bootstrap";
 import Image from "next/image";
 import { AdminLayout } from "../components/Layout";
 import styles from "../scss/Manage.module.scss";
+import { GetServerSideProps, NextPage } from "next";
+import { _getProducts } from "@/config/api";
+import { Product } from "@/types";
+import { useState, ChangeEvent } from "react";
 
-export default function Manage() {
+interface ManageProps {
+  data: Product[];
+  error?: string;
+  e?: any;
+}
+
+const Manage: NextPage<ManageProps> = ({ data, error, e }) => {
+  if (error) alert(error);
+  if (e) console.log(e);
+
+  const [products, setProducts] = useState<Product[]>(data || []);
+
+  const updateStock = (e: ChangeEvent<HTMLInputElement>, index: number) => {
+    const newValue: number = parseInt(e.target.value);
+    products[index].stock = newValue;
+    setProducts([...products]);
+  };
+
   return (
     <AdminLayout>
       <Container className="my-4 font-roboto">
@@ -17,65 +38,56 @@ export default function Manage() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>
-                <Row>
-                  <Col lg={2}>
-                    <Image
-                      src="https://placeimg.com/640/480/people"
-                      width={200}
-                      height={200}
-                    />
-                  </Col>
-                  <Col lg={10}>
-                    <h2>Lorem ipsum dolor sit amet.</h2>
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Facere, obcaecati! Laborum voluptatum quidem odio suscipit
-                      nam nisi sunt a hic.
-                    </p>
-                    <span className="text-muted">Men Clothing</span>
-                  </Col>
-                </Row>
-              </td>
-              <td className={styles.custom_input}>
-                <Form.Control type="text" />
-              </td>
-              <td className="align-middle">
-                <button className={styles.custom_btn}>Update</button>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <Row>
-                  <Col lg={2}>
-                    <Image
-                      src="https://placeimg.com/640/480/people"
-                      width={200}
-                      height={200}
-                    />
-                  </Col>
-                  <Col lg={10}>
-                    <h2>Lorem ipsum dolor sit amet.</h2>
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Facere, obcaecati! Laborum voluptatum quidem odio suscipit
-                      nam nisi sunt a hic.
-                    </p>
-                    <span className="text-muted">Women Clothing</span>
-                  </Col>
-                </Row>
-              </td>
-              <td className={styles.custom_input}>
-                <Form.Control type="text" />
-              </td>
-              <td className="align-middle">
-                <button className={styles.custom_btn}>Update</button>
-              </td>
-            </tr>
+            {products.map((product, index) => (
+              <tr key={product._id}>
+                <td>
+                  <Row>
+                    <Col lg={2}>
+                      <Image
+                        src={
+                          product.image || "https://placeimg.com/640/480/people"
+                        }
+                        width={200}
+                        height={200}
+                      />
+                    </Col>
+                    <Col lg={10}>
+                      <h2>{product.name}</h2>
+                      <p>{product.description}</p>
+                      <span className="text-muted">{product.category}</span>
+                    </Col>
+                  </Row>
+                </td>
+                <td className={styles.custom_input}>
+                  <Form.Control
+                    onChange={(e) => updateStock(e, index)}
+                    value={product.stock}
+                    type="number"
+                  />
+                </td>
+                <td className="align-middle">
+                  <button className={styles.custom_btn}>Update</button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </Table>
       </Container>
     </AdminLayout>
   );
-}
+};
+
+export default Manage;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  try {
+    const data: any = await _getProducts();
+
+    if (data.error)
+      return { props: { data: [], error: data.error, e: data.e || "" } };
+
+    return { props: { data: data } };
+  } catch (error) {
+    return { props: { data: [], error } };
+  }
+};
